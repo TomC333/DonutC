2cc296e3ee8257499e744e05b8a9d4167b08084a
TomC333 <ddadi21@freeuni.edu.ge>
2024-01-09 17:56:16 +0400

    Donut is implemented, but there are some rough numbers which should be changed :D

--- main.c
+++ main.c
@@ -2,37 +2,122 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <string.h>
+#include <math.h>
 
-void drawSquare(int size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            printf("* ");
-        }
-        printf("\n");
-    }
-}
 
+// Torus ring radius 
+#define R1 2
+// Distance from torus ring center to rotating axis
+#define R2 5
+// STOD is distance from screen to "3D" donut
+#define STOD 100
+// Field of view should be calculated according to upper values, but for now some random value :D (which fits good to my screen :D) (should be changed )
+#define FOV 100*STOD/(R1+R2)*0.5
+// Alpha is corner which helps to get ring point
+#define ALPHA_SPACING 0.02
+// Theta is corner which goes around 3D object + used to rotate on y
+#define THETA_SPACING 0.08
+// Just simplify using of PI :D 
+#define PI M_PI
+
+
+typedef struct{
+    int height;
+    int width;
+} dimensions;
+
+
+dimensions getTerminalDimensions();
 
 int main() {
     
+    dimensions terminalDimensions = getTerminalDimensions();
 
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    terminalDimensions.width = 100;
+    terminalDimensions.height =  100;
 
-    int terminalWidth = w.ws_col;
-    int terminalHeight = w.ws_row;
+    char symbols[terminalDimensions.width][terminalDimensions.height + 10];
+    int zBuffer[terminalDimensions.width][terminalDimensions.height + 10];
+    
+    // We need 2 more corners for x an z rotations, alpha and theta should find all point mappings always, 
+    // but that 2 value can be changed after one movement is finished 
+    double xRotation = 0;
+    double zRotation = 0;
+    
+    while(1){
 
-    int squareSize = 10;
+        memset(symbols, ' ', sizeof(symbols));
+        memset(zBuffer, 0, sizeof(zBuffer));
 
-    for(int i = 0; i < 10; i++){
-         int startX = (terminalWidth - squareSize) / 2;
-        int startY = (terminalHeight - squareSize) / 2;
-        system("clear");
-        printf("\033[%d;%dH", startY, startX);
-        drawSquare(squareSize);
-        squareSize += 1;
+        double xRotationCos = cos(xRotation);
+        double zRotationCos = cos(zRotation);
+
+        double xRotationSin = sin(xRotation);
+        double zRotationSin = sin(zRotation);
+        
+        for(double alpha = 0; alpha < 2 * PI; alpha += ALPHA_SPACING){
+            
+            double sinAlpha = sin(alpha);
+            double cosAlpha = cos(alpha);
+
+            double circleX = R2 + R1 * cosAlpha;
+            double circleY = R1 * sinAlpha;
+
+            for(double theta = 0; theta < 2 * PI; theta += THETA_SPACING){  
+                
+                double sinTheta = sin(theta);
+                double cosTheta = cos(theta);
+
+                double x = circleX * (zRotationCos * cosTheta + xRotationSin * zRotationSin * sinTheta) - circleY * xRotationCos * zRotationSin;
+                double y = circleX * (zRotationSin * cosTheta - zRotationCos * xRotationSin * sinTheta) + circleY * xRotationCos * zRotationCos;
+                double z = STOD + circleX * xRotationCos * sinTheta + circleY * xRotationSin;
+
+                // lets take (0, 1, -1) as light direction and make dot product of it and surface normal, to determine light strength and direction
+                // light vector is (0, 1, -1) and it's not normalized so value of light is from -sqrt(2) to sqrt(2), therefore I divede l with sqrt(2)
+                double light = cosTheta * cosAlpha * zRotationSin - xRotationCos * cosAlpha * sinTheta - xRotationSin * sinAlpha + zRotationCos * (xRotationCos * sinAlpha - cosAlpha * xRotationSin * sinTheta);
+                // new range from -1 to 1
+                light /= sqrt(2);
+
+                int onScreenX = 10 + (terminalDimensions.height - (int) ((FOV * x) / (STOD + x))) / 2;
+                int onScreenY = 10 + (terminalDimensions.width - (int) ((FOV * y) / (STOD + z))) / 2;
+
+                if(light > 0){
+                    
+                    if(1 / z > zBuffer[onScreenX][onScreenY]){
+                        
+                        zBuffer[onScreenX][onScreenY] = 1 / z;
+                        symbols[onScreenX][onScreenY] = ".,-~:;=!*#$@"[(int)(light * 12)];
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < terminalDimensions.height; i++){
+            for(int j = 0; j < terminalDimensions.width; j++){
+                printf("%c", symbols[j][i]);
+            }
+            printf("\n");
+        }
+
+        printf("\r");
+        xRotation += 0.1;
+        zRotation += 0.1;
+        usleep(100000);
     }
 
     return 0;
    
+}
+
+dimensions getTerminalDimensions(){
+    
+    struct winsize w;
+    dimensions terminalDimensions;
+    
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    terminalDimensions.width = w.ws_col;
+    terminalDimensions.height = w.ws_row;
+    
+    return terminalDimensions;
 }
